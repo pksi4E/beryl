44240c7c7b6cd0aeef538b5bc158a836d041ed39
pksi4E <p.ksiazak@gmail.com>
2024-01-24 02:08:02 +0100

    modified findinverse function and added eea function

--- numbel.lua
+++ numbel.lua
@@ -112,9 +112,48 @@ function findinverse(x, n)
 	to number x. Algorithm is based on
 	Extended Euclid algorithm.
 ]]
-
-	-- to be done
+	if isreversible(x, n) then
+		g, s, t = eea(x, n)
+		if s < 0 then
+			return n + s
+		else
+			return s
+		end
+	else
+		return 'not reversible'
+	end
 end
+
+function eea (a, b)
+--[[
+	Function...
+]]--
+	a0 = a
+	b0 = b
+	t0 = 0
+	t = 1
+	s0 = 1
+	s = 0
+	q = a0 // b0
+	r = a0 - q * b0
+	while r > 0 do
+		temp = t0 - q * t
+		t0 = t
+		t = temp
+		temp = s0 - q * s
+		s0 = s
+		s = temp
+		a0 = b0
+		b0 = r
+		q = a0 // b0
+		r = a0 - q * b0
+	end
+	r = b0
+	return r, s, t
+end
+a, b = 200, 136
+r,s,t = eea(a, b)
+io.write(string.format("%d = nwd(%d, %d) = %d * a + %d * b = %d", r, a, b, s, t, r))
 
 -- arg1 = tonumber(arg[1])
 -- arg2 = tonumber(arg[2])
